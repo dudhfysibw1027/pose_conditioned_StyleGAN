48eb07eefe55d01267b9220c5b3303270dd5c448
daniel <58432350+dudhfysibw1027@users.noreply.github.com>
2022-11-12 20:50:49 +0800

    Update main.py

--- stylegan2-ada-pytorch/training/main.py
+++ stylegan2-ada-pytorch/training/main.py
@@ -5,13 +5,14 @@ import torch.utils.data as data
 from parsing_generation_segm_attr_dataset import ParsingGenerationDeepFashionAttrSegmDataset
 # from pose_attr_dataset import DeepFashionAttrPoseDataset
 from pose_encoder import ShapeAttrEmbedding, PoseEncoder
-
+import numpy as np
 
 class main2:
   def __init__(
     self
     #train_dataset,
     #train_loader,
+    
   ):
     self.train_dataset = ParsingGenerationDeepFashionAttrSegmDataset(
         segm_dir='/content/drive/MyDrive/padded_dataset_unzip/padded_dataset/padded_segm',
@@ -25,24 +26,24 @@ class main2:
         num_workers=4,
         drop_last=True
     )
-    def main2():
-    # my_item_dict = train_loader[0]
-      for i, data_ in enumerate(self.train_loader):
-          if i > 1:
-              break
-          my_item_dict = data_
+  def main3(self):
+  # my_item_dict = train_loader[0]
+    for i, data_ in enumerate(self.train_loader):
+        if i > 1:
+          break
+        my_item_dict = data_
 
-      my_pose_encoder = PoseEncoder(size=512)
+    my_pose_encoder = PoseEncoder(size=512)
 
-      my_attr_embedder = ShapeAttrEmbedding(dim=8, out_dim=128,
+    my_attr_embedder = ShapeAttrEmbedding(dim=8, out_dim=128,
                                           cls_num_list=[2, 4, 6, 5, 4, 3, 5, 5, 3, 2, 2, 2, 2, 2, 2])
 
       # print(my_item_dict)
-      my_attr_embedding = my_attr_embedder(my_item_dict["attr"])
+    my_attr_embedding = my_attr_embedder(my_item_dict["attr"])
 
-      x = my_pose_encoder(input=my_item_dict["densepose"], attr_embedding=my_attr_embedding)
-
-      return x
+    x = my_pose_encoder(input=my_item_dict["densepose"], attr_embedding=my_attr_embedding)
+    #x = np.array(x)
+    return x
 
 
 # Press the green button in the gutter to run the script.
